1e1b3f6a71f571fa1af988b7f094b4a326892ae8
chendom <772571631@qq.com>
2019-01-18 17:10:44 +0800

    提交notify和wait的例子代码...1

---
+++ src/main/java/chapter_5/com/flexible/Demo.java
@@ -0,0 +1,15 @@
+package chapter_5.com.flexible;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: chendom
+ * Date: 2019-01-17
+ * Time: 12:30
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("---------ok---------------");
+    }
+}
